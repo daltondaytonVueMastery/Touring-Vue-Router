907716e615d9f91d0767ecccfba1d9d72a516871
Dalton Dayton <daltondayton1@gmail.com>
2022-03-20 13:44:05 -0600

    init

---
+++ src/views/EventList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="events">
+    <h1>Events For Good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard, // register it as a child component
+  },
+  computed: {
+    events() {
+      return this.$store.state.events;
+    },
+  },
+  created() {
+    this.$store.dispatch("fetchEvents").catch((error) => {
+      this.$router.push({
+        name: "ErrorDisplay",
+        params: { error: error },
+      });
+    });
+  },
+};
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
